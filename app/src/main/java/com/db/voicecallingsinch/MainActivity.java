9d3467f697cc45a5750a3c40e90b6d0eafd2cbe8
Satyam Prajapati <satyam7355pra@gmail.com>
2022-07-24 10:00:53 -0700

    added to firebase

--- app/src/main/java/com/db/voicecallingsinch/MainActivity.java
+++ app/src/main/java/com/db/voicecallingsinch/MainActivity.java
@@ -3,12 +3,28 @@ package com.db.voicecallingsinch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etEmail,etPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
